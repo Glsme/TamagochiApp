8f98da9fc229be7c556cf829f08df37ec603c51a
Glsme <glasses.str.man@gmail.com>
2022-07-24 16:26:09 +0900

    Feat: IQKeyboardManagerSwift Library 추가, 키보드 출현 시 view가 올라가는 코드 추가

--- TamagochiApp/Growth/GrowthViewController.swift
+++ TamagochiApp/Growth/GrowthViewController.swift
@@ -15,8 +15,13 @@ class GrowthViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var growthLabel: UILabel!
     @IBOutlet weak var SelectedImageView: UIImageView!
+    @IBOutlet weak var riceTextField: UITextField!
+    @IBOutlet weak var waterTextField: UITextField!
     
     var name: String?
+    var level = 1
+    var rice = 0
+    var water = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +30,42 @@ class GrowthViewController: UIViewController {
         
         UISetting.setNameLabelUI(label: titleLabel)
         titleLabel.text = name
+        
+        growthLabel.textColor = UISetting.fontColor
+        growthLabel.font = .systemFont(ofSize: 13, weight: .semibold)
+        growthLabel.text = "LV\(level) ∙ 밥알 \(rice)개 ∙ 물방울 \(water)개"
+        
     }
 
+    @IBAction func riceButtonClicked(_ sender: UIButton) {
+        if riceTextField.text! == "" {
+            rice += 1
+            
+        } else {
+            if Int(riceTextField.text!) != nil {
+                rice += Int(riceTextField.text!)!
+            } else {
+                riceTextField.placeholder = "숫자만 입력해주세요"
+            }
+        }
+        riceTextField.text = ""
+        growthLabel.text = "LV\(level) ∙ 밥알 \(rice)개 ∙ 물방울 \(water)개"
+    }
+    
+    @IBAction func waterButtonClicked(_ sender: UIButton) {
+        if waterTextField.text! == "" {
+            water += 1
+        } else {
+            if Int(riceTextField.text!) != nil {
+                rice += Int(riceTextField.text!)!
+            } else {
+                riceTextField.placeholder = "숫자만 입력해주세요"
+            }
+        }
+        
+        growthLabel.text = "LV\(level) ∙ 밥알 \(rice)개 ∙ 물방울 \(water)개"
+    }
+    @IBAction func tapGestureRecognized(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
 }
